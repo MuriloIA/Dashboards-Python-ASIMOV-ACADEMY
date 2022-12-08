6b714920073bf183331c48ea01401438413d5db3
MuriloIA <musilva.14@gmail.com>
2022-12-08 12:55:10 -0300

    Adicionando os parâmetros do choropleth_mapbox (mapa).

--- DASHBOARD-COVID-19/app.py
+++ DASHBOARD-COVID-19/app.py
@@ -22,10 +22,7 @@ from dash import Dash, html, dcc, Input, Output
 # Carga dos dados
 df_states = pd.read_csv("https://raw.githubusercontent.com/asimov-academy/Dashboards/main/dashboard-covid-19/df_states.csv")
 df_brasil = pd.read_csv("https://raw.githubusercontent.com/asimov-academy/Dashboards/main/dashboard-covid-19/df_brasil.csv")
-
 brazil_states = json.load(open("geojson/brazil_geo.json", "r"))
-
-brazil_states["features"][0].keys()
 
 df_states_ = df_states[df_states["data"] == "2020-05-13"]
 select_columns = {"casosAcumulado": "Casos Acumulados", 
@@ -33,14 +30,28 @@ select_columns = {"casosAcumulado": "Casos Acumulados",
                 "obitosAcumulado": "Óbitos Totais",
                 "obitosNovos": "Óbitos por dia"}
 
+
+#################################################################################
+# -= GRÁFICOS =- #
+
+# Gráfico de mapa
+fig = px.choropleth_mapbox(
+    data_frame=df_states_, locations="estado", color="casosNovos", geojson=brazil_states, 
+    color_continuous_scale="Redor", opacity=0.4, hover_data={"casosAcumulado": True, 
+    "casosNovos": True, "obitosNovos": True, "estado": True}, center={"lat": -16.95, "lon": -47.78}
+)
+
+
 #################################################################################
 # -= LAYOUT DO DASHBOARD =- #
 
 # Instanciando o Dash
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
 # Criando Layout
 app.layout = dbc.Container(children=[
+
+
 
 ], fluid=True, style={"height": "100%"})
 
